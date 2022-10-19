81b90625ea95bddc2a2997f34cec67c36c610d95
Willian Silveira Espindola <87204387+Willespindola@users.noreply.github.com>
2022-10-19 01:33:49 -0300

    Create text-pagination.php
    Este código foi feito para fazer a paginação automática de um texto.

---
+++ text-pagination.php
@@ -0,0 +1,200 @@
+<?php
+$url = substr($_SERVER["REQUEST_URI"], strpos($_SERVER["REQUEST_URI"], '/')+1);
+$mat = str_replace('blog/materias-blog/', '', $url);
+
+$texto ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, atque expedita quis autem provident quos officiis illo sit culpa incidunt magni unde ipsa repellat corrupti ipsam doloribus quo voluptas fugit!';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php include __DIR__ . '/includes/head.php'; ?>
+  </head>
+  <body>
+    <main class="fadeIn">
+      <!-- Header -->
+      <?php include __DIR__ . '/includes/header.php';?>
+      <!-- Galeria -->
+      <section class="gallery">
+        <?php
+        $database->SetMenu('blog/banner-blog/', '1');
+        $database->SetIniciarRegistro(0);
+        $database->SetItensPag(1); //Quantos itens
+        $database->setTipoFiltro('posicao');
+        $database->SetOrder('ASC');
+        $database->paginas(base64_encode('
+          <!--repeteA-->
+          <figure><img src="fotoGrandeSYS" alt="tituloSYS" width="100%" height="auto"></figure>
+          <!--repeteB-->
+        '));
+        ?>
+      </section>    
+        <div class="container mt-5">
+          <div class="row">
+            <!-- Conteúdo -->
+            <?php
+            $database->SetMenu($database->parametros['menuRoteador'],'1');
+            ?>
+              <div class="col-12 col-md-6 col-lg-8">
+                <h1 class="blogTitle"><?php $database->interno(base64_encode('tituloSYS')); ?></h1>
+                <h3 class="blogSubtitle"><?php $database->interno(base64_encode('campoOpcionalClearSYS')); ?></h3>
+                <!-- <div class=""> -->
+                  <?php
+                  $textoString = implode($texto);       
+                  $var = $textoString;
+                  $caracter = '=P=';
+                  $lim = 2000;
+	                $tamanho = strlen($var);
+	                if($tamanho > $lim){	
+		                $quebra = $tamanho/$lim;
+		                $ini = 0;
+		                $fim = $lim;
+	
+		                for($i=0; $i <= intval($quebra); $i++){
+			                if($i == intval($quebra)){
+				                $nova.= substr($var, $ini, $lim);
+                      }else{
+				                $nova.= substr($var, $ini, $lim).$caracter;
+			                  $ini = $fim;
+			                  $fim = $fim+$lim;
+                      }
+		                }
+		                $textoCompleto = explode("=P=", $nova , -1);
+                    
+	                } else {
+		                $textoCompleto = $var;
+                    
+	                }
+                  ?> 
+                  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+    	                <?php
+    		              $controle_ativo = 2;		
+    		              $controle_num_slide = 1;
+    		              foreach ($textoCompleto as $key => $textoAtual) { 
+    			              if($controle_ativo == 2){ ?>
+    				              <li data-target="#carouselExampleControls" data-slide-to="0" class="active indicadores"></li><?php
+    				              $controle_ativo = 1;
+    			              }else{ ?>
+    				              <li data-target="#carouselExampleControls" data-slide-to="<?php echo $controle_num_slide; ?>"></li><?php
+    				              $controle_num_slide++;
+    			              }
+    		              }
+    	                ?>						
+                    </ol>
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                      <?php
+                      $controle_ativo = 2;
+                      foreach ($textoCompleto as $key => $textoAtual) {
+                          if($controle_ativo == 2){ ?>
+                          <div class="carousel-item active" id="imagem_<?= $textoAtual ?>">
+                          <p class="blogText"><?= $textoAtual; ?></p>
+                          </div><?php
+                          $controle_ativo = 1;
+                      }else{ ?>
+                          <div class="carousel-item" id="imagem_<?= $textoAtual ?>">
+                          <p class="blogText"><?= $textoAtual; ?></p>
+                          </div><?php
+                          }
+                          ?>
+                      <?php
+                      }
+                      ?>
+                    </div>
+                  </div>  
+                  <!-- Galeria -->
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X
+                    +965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" 
+                    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" 
+                    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                <!-- </div> -->
+
+                <div class="blogPagination box-paginacao">
+                  <a class="carousel-control-prev btn-paginacao" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <button type="button" class="blogPagination__button btnReset btn-paginacao" title="Ir para página anterior"
+                      aria-label="Ir para página anterior"><b><i class="fa-solid fa-angle-left"></i> página anterior</b>
+                    </button>
+                  </a>
+                  <a class="carousel-control-next c-color-5 btn-paginacao" href="#carouselExampleControls" role="button" data-slide="next">
+                    <button type="button" class="btn-paginacao blogPagination__button btnReset" title="Ir para próxima página"
+                      aria-label="Ir para próxima página"><b>próxima página <i class="fa-solid fa-angle-right"></i></b>
+                    </button>
+                  </a>
+                </div>
+              </div>
+            <!-- Cards Blog -->
+            <div class="col-12 col-md-6 col-lg-3 offset-lg-1 box-ler-mais">
+              <div class="row">
+                <!-- Card Blog -->
+                <?php
+                $database->SetMenu('/blog/materias-blog', '1');
+                $database->SetIniciarRegistro(0);
+                $database->setLimiteDescricao(100);
+                $database->SetItensPag(2); //Quantos itens
+                $database->setTipoFiltro('posicao');
+                $database->setLimiteTitulo(100);
+                $database->SetOrder('ASC');
+                $database->paginas(base64_encode('
+                  <!--repeteA-->
+                  <aside class="col-12 mb-5">
+                    <div class="cardBlog cardShadow zoomIn" title="Ir para Blog" aria-label="Ir para Blog">
+                      <figure>
+                        <a href="/paginaSYSurlSYS"><img src="fotoGrandeSYS" alt="tituloSYS" width="100% height="auto"></a>
+                      </figure>
+                      <div class="cardBlog__content">
+                        <h3 class="cardBlog__title"><a href="/paginaSYSurlSYS">tituloSYS</a></h3>
+                        <div class="cardAgenda__text">
+                          descricaoSYS
+                        </div>
+                        <a href="/paginaSYSurlSYS" type="button" class="ml-auto cardBlog__link" aria-label="Leia mais"  title="Leia mais">Leia mais <i class="fa-solid fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </aside>
+                  <!--repeteB-->
+                '));
+                  ?>  
+              </div>
+            </div>
+          </div>
+      </div>
+    </main>
+    <script>
+      window.scrollTo({
+    top: 800,
+    left: 0,
+    behavior: 'smooth'
+  }), {delay: 1000};
+  </script>
+    <?php include __DIR__ . '/includes/footer.php'; ?>
+    
+    <?php include __DIR__ . '/includes/scripts.php'; ?>
+  </body>
+</html>
